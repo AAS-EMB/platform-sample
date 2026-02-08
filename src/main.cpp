c20faed269a05c8cd59ad6fe0d68391ef66c4dc1
Anton Anosov <31388218+AAS-EMB@users.noreply.github.com>
2026-02-08 13:50:21 +0300

    feat: Implemented example with blink leds for f1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <platforms/hw.hpp>
+#include <coro/coro_task.hpp>
+#include <coro/async_flag.hpp>
+#include <coro/coro_timer.hpp>
+
+using namespace driver;
+using namespace std::chrono_literals;
+
+static uint8_t buf[1024];
+static std::pmr::monotonic_buffer_resource pool(buf, sizeof(buf));
+
+static async::coro_timer coro_timer_led3{ 1s };
+static async::coro_timer coro_timer_led4{ 5s };
+
+async::coro_task blink_led3() noexcept
+{
+    while (true)
+    {
+        coro_timer_led3.expires_after(1s);
+        co_await coro_timer_led3.async_wait();
+        platform::hw_instance::led3().toggle();
+    }
+}
+
+async::coro_task blink_led4() noexcept
+{
+    while (true)
+    {
+        coro_timer_led4.expires_after(5s);
+        co_await coro_timer_led4.async_wait();
+        platform::hw_instance::led4().toggle();
+    }
+}
 
 int main() {
-    using namespace std::chrono_literals;
+    async::coro_task::set_resource(pool);
 
     auto res = platform::hw_init();
 
-    auto last = driver::steady_clock::now();
+    async::launch_task(blink_led3).resume();
+    async::launch_task(blink_led4).resume();
 
-    while (true) {
-        if (driver::steady_clock::now() - last >= 1s) {
-          platform::hw_instance::led3().toggle();
-          platform::hw_instance::led4().toggle();
-          last = driver::steady_clock::now();
-        }
+    while (true)
+    {
+        coro_timer_led3.process();
+        coro_timer_led4.process();
     }
 
     return 0;
